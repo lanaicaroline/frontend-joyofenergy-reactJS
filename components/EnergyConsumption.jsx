f856d8fba2aa34f663fab1e29b9772e0b0e78adc
Caroline canale <carol@MacBook-Air-de-Caroline.local>
2025-08-12 21:09:05 -0300

    (refactor) energyconsuption, usegetdata, mock chart.js: add button to 90 days

--- components/EnergyConsumption.jsx
+++ components/EnergyConsumption.jsx
@@ -13,17 +13,18 @@ import useGetData from "../hooks/useGetData/useGetData.js";
  */
 
 export const EnergyConsumption = ({ readings }) => {
-  const [chartData, setChartData] = React.useState([]);
-  const { data } = useGetData();
-
   const containerId = "usageChart";
+
+  const [chartData, setChartData] = React.useState([]);
+  const [timePeriod, setTimePeriod] = React.useState(30);
+
+  const { data } = useGetData(timePeriod);
 
   // 1. Crie o mock de uma api para trazer esse data
   useEffect(() => {
     setChartData(data?.datasets);
     renderChart(containerId, data?.datasets)
   }, [data, chartData]);
-  // ===============================================
 
   return (
     <>
@@ -44,8 +45,35 @@ export const EnergyConsumption = ({ readings }) => {
               white
               bold
             "
+          onClick={() => {
+            setTimePeriod(30);
+          }}
+          type="button"
         >
           Last 30 days
+        </button>
+        <button
+          className="
+              h5
+              inline-block
+              shadow-2
+              pl2
+              pr2
+              pt1
+              pb1
+              roundedMore
+              border-grey
+              bg-blue
+              white
+              bold
+            "
+            onClick={() => {
+              // 2. Como você faria para tornar o botão “Last 30 days” funcional para filtrar períodos diferentes (ex: 7, 30, 90 dias)?
+              setTimePeriod(90);
+            }}
+            type="button"
+        >
+          Last 90 days
         </button>
       </section>
       <section className="chartHeight mb3">
--- hooks/useGetData/useGetData.ts
+++ hooks/useGetData/useGetData.ts
@@ -1,18 +1,24 @@
 import { useState, useEffect } from "react";
-import mockData from '../../__mocks__/chart';
+import { mockDataMonth, mockDataThreeMonths } from '../../__mocks__/chart';
 import { ChartData } from './types';
 
-const useGetData = () => {
+const useGetData = (period: number) => {
   const [data, setData] = useState<ChartData | null>(null);
 
   const fetchData = async () => {
-    const response = await mockData();
-    setData(response.data);
+    if (period !== 30) {
+      const response = await mockDataThreeMonths();
+      setData(response.data);
+    } else {
+      const response = await mockDataMonth();
+      setData(response.data);
+    }
   };
+  
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [period]);
 
   return { data };
 };
